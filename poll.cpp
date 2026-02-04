bdb522a6378a4f0bd3eea1bb7b893263be505f20
martimsaunders <martimprazeres13@gmail.com>
2026-02-04 16:00:21 +0000

    test.cpp

---
+++ poll.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   poll.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: praders <praders@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/04 12:10:00 by praders           #+#    #+#             */
+/*   Updated: 2026/02/04 12:15:24 by praders          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+#include <cerrno>
+#include <unistd.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+const char die(const char *msg){
+	std::cerr << msg << ":" << std::strerror(errno) << std::endl;
+	std::exit(1);
+}
+
+
+static int setNonBlocking(int fd){
+	int flags = fcntl(fd, F_GETFL, 0);
+	if (flags == -1) return -1;
+	if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1) return -1;
+	return 0;
+}
+
