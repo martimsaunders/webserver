42249ab1d668604057a086f89c227e88e967742d
icrackfofun <miguelred2009@gmail.com>
2026-02-23 23:36:20 +0000

    update file service

--- inc/request/CGIHandler.hpp
+++ inc/request/CGIHandler.hpp
@@ -1,14 +1,22 @@
 #pragma once
 #include "HttpRequest.hpp"
+#include "../internal/FileService.hpp"
 #include "../response/HttpResponse.hpp"
 #include "../config/LocationConfig.hpp"
+#include "../config/ServerConfig.hpp"
 
 class CGIHandler
 {
 public:
-    HttpResponse execute(
+    static HttpResponse execute(
         const HttpRequest& request,
         const Location& location,
-        const std::string& scriptPath
+        const std::string& fullPath,
+        const FileInfo& info,
+        const ServerConfig& serverConfig
     );
+
+private:
+    static std::string extractExtension(const std::string& path);
+    static std::string resolveInterpreter(const Location& location, const std::string& ext);
 };
