059e588c4263997d20ebcc881de383452da8e7ec
martimsaunders <martimprazeres13@gmail.com>
2026-03-04 22:30:19 +0000

    100% availability em tudo

--- cgi-bin/ok.py
+++ cgi-bin/ok.py
@@ -1,2 +1,4 @@
 #!/usr/bin/env python3
+print("Content-Type: text/plain")
+print()
 print("ok")
---
+++ cgi-bin/sleep.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import time
+time.sleep(35)
+print("Content-Type: text/plain\r\n")
+print("OK")
